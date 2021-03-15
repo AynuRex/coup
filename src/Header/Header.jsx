aae718160cad7b58b16356905aa8115938a32721
AynuRex <badretdinov12@gmail.com>
2021-03-15 18:40:02 +0300

    addded header and footer components

---
+++ src/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './Header.module.css'
+
+const Header = (props)=>{
+
+    return (
+        <div className={style.header}>
+        <div>
+            COUPE
+        </div>
+        <div  >
+            GETTING STARTED
+        </div>
+        <div  >
+            COUPE
+        </div>
+    </div>)
+}
+
+export default Header;
