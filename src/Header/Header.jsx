06ee1fa43590a4cb9e9afbbfa39b8b3fb8a7e05f
AynuRex <badretdinov12@gmail.com>
2021-03-16 13:40:53 +0300

    improved footer style/ header small fix

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = (props) => {
     return (
         <div className="header">
-            <div className=" ">COUPE</div>
+            <div className=" ">COUP</div>
             <HeaderMenu/>
         </div>
     )
